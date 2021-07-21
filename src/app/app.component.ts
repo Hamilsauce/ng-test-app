9d487ed00d5f7cc147b53ceae9e8a5884fd3b3c7
jacob126 <jacob@theleaguestock.com>
2021-07-20 21:16:51 -0700

    added chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
-import { Observable } from "rxjs";
-import { AppChildComponent} from './app-child/app-child.component'
-import { ObservableTestComponent } from "./observable-test/observable-test.component";
+import { Observable } from 'rxjs';
+import { AppChildComponent } from './app-child/app-child.component';
+import { ObservableTestComponent } from './observable-test/observable-test.component';
+import * as Highcharts from 'highcharts';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ng-test-app';
   things: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
   clickCount: number = 6;
   obs: Observable<string>;
+
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {
+    series: [
+      {
+        data: [1, 2, 3],
+        type: 'column',
+      },
+    ],
+  };
 
   updateClickCount(amt: number) {
     console.log('test');
@@ -24,10 +36,10 @@ export class AppComponent {
   addNewThing() {
     const newThing = this.things[this.things.length - 1] + 1;
     console.log(newThing);
-    this.things.push(newThing)
+    this.things.push(newThing);
   }
 
   deleteLastThing() {
-    this.things.pop()
+    this.things.pop();
   }
 }
